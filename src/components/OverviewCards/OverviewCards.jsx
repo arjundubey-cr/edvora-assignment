ab405b5a2a3681b157ff39783964a3b7c4e49cb7
Arjun Dubey <arjundubeycr@gmail.com>
2022-09-12 15:37:30 +0530

    final commit

--- src/components/OverviewCards/OverviewCards.jsx
+++ src/components/OverviewCards/OverviewCards.jsx
@@ -1,13 +1,18 @@
 import Card from "./Card/Card";
 
 const OverviewCards = ({ products, users, orders }) => {
-  return (
-    <div className="grid grid-cols-3 gap-2">
-      <Card data={Object.keys(products).length} heading="Total Products"/>
-      <Card data={Object.keys(users).length} heading="Total Users"/>
-      <Card data={orders.length} heading="Total Orders"/>
-    </div>
-  );
+  if (products && users && orders) {
+    return (
+      <div className="grid grid-cols-3 gap-2">
+        <Card
+          data={products && Object.keys(products).length}
+          heading="Total Products"
+        />
+        <Card data={users && Object.keys(users).length} heading="Total Users" />
+        <Card data={orders.length} heading="Total Orders" />
+      </div>
+    );
+  }
 };
 
 export default OverviewCards;
